dd0982a3ada444205065886dc15f5bc7142bda36
GustavoMF31 <gustavodemendoncafreire@gmail.com>
2019-01-28 11:57:10 -0200

    Initial commit

---
+++ solver.py
@@ -0,0 +1,195 @@
+from copy import deepcopy
+
+INTEGERS = [1,2,3,4,5,6,7,8,9]
+
+class SudokuException(Exception):
+    """Basic Sudoku exception"""
+    pass
+
+class UnsolvableSudokuException(SudokuException):
+    """Raised by an unsolvable Sudoku"""
+    pass
+
+class InvalidSudokuException(SudokuException):
+    """Raised by a Sudoku with an invalid structure"""
+    pass
+
+class SudokuBoard:
+    def __init__(self, rows):
+        if len(rows) != 9:
+            raise InvalidSudokuException(
+                f"A Sudoku needs 9 rows, but {len(rows)} found")
+
+        for index, row in enumerate(rows):
+            if len(row) != 9:
+                raise InvalidSudokuException(
+                    f"""A Sudoku needs a combination of
+                    numbers or 'None's totalizing 9 per row,
+                    but got {len(row)} in row {row} at index {index}""")
+        
+        self.rows = rows
+
+    def is_complete(self):
+        #True if the sudoku is completely filled with numbers
+        #Not necessarily solved
+        
+        for row in self.rows:
+            for number in row:
+                if number is None:
+                    return False
+        return True
+
+    @staticmethod
+    def subgridize(three_rows):
+        #Transforms three rows into three subgrids
+        #111 222 333    111111111
+        #111 222 333 -> 222222222
+        #111 222 333    333333333
+        
+        subgrids = [[],[],[]]
+        
+        if len(three_rows) != 3:
+            raise ValueError(f"Subgridize needs 3 rows, but got {len(three_rows)}")
+
+        for subgrid_index in range(3):
+            for row in three_rows:
+                for num in row[subgrid_index * 3:(subgrid_index + 1) * 3]:
+                    subgrids[subgrid_index].append(num)
+
+        return subgrids
+    
+    def subgrids(self):
+        #Returns the board, reorganized into 3x3 subgrids
+        subgrids = []
+
+        #Iterate through the rows in groups of three
+        for three_rows in [[self.rows[i],
+                            self.rows[i+1],
+                            self.rows[i+2]]
+                           for i in range(0, 9 ,3)]:
+
+            #Add each subgrid to the subgrids list
+            for row in self.subgridize(three_rows):
+                subgrids.append(row)
+        
+        return subgrids
+
+    def cols(self):
+        #Returns the board, reorganized into columns
+        cols = [[],[],[],[],[],[],[],[],[]]
+        
+        for i in range(9):
+            for row in self.rows:
+                cols[i].append(row[i])
+
+        return cols
+    
+    def is_solved(self):
+        #True if the Sudoku is solved
+        
+        for row in self.rows:
+            for integer in INTEGERS:
+                if row.count(integer) != 1:
+                    return False
+
+        for col in self.cols():
+            for integer in INTEGERS:
+                if col.count(integer) != 1:
+                    return False
+
+        for subgrid in self.subgrids():
+            for integer in INTEGERS:
+                if subgrid.count(integer) != 1:
+                    return False
+
+        return True
+
+    def possible_numbers(self, y, x):
+        #Returns wich numbers could occupy a specific tile
+
+        #Possible optimization:
+        #Cache the columns and the subgrids
+        possible_numbers = []
+
+        #Add to the list each number that is
+        for num in INTEGERS:
+            #Not in the row
+            if (num not in self.rows[y] and
+                   #Not in  the column
+                   num not in self.cols()[x] and
+                   #Not in the subgrid
+                   num not in self.subgrids()[x // 3 + (y // 3) * 3]):
+                   
+                possible_numbers.append(num)
+
+        return possible_numbers
+            
+            
+    def possibilities(self):
+        #Returns the possible numbers for each tile
+        possibilities = {}
+        
+        for y in range(9):
+            for x in range(9):
+                if self.rows[y][x] is None:
+                    possible_numbers = self.possible_numbers(y, x)
+                    possibilities[y, x] = possible_numbers
+
+        return possibilities
+
+    def solved(self):
+        #Returns a solved version of the board
+        
+        least_possibilities_tile = () #(y, x)
+        least_possibilities_tile_possibility_count = 10
+        least_possibilities_tile_possibilities = []
+
+        #For each empty tile
+        for y in range(9):
+            for x in range(9):
+                if self.rows[y][x] is None:
+
+                    #Get it's possible numbers
+                    possible_numbers = self.possible_numbers(y, x)
+                    possibility_count = len(possible_numbers)
+
+                    #If no number is possible, it's unsolvable
+                    if possibility_count == 0:
+                        raise UnsolvableSudokuException
+                    
+                    #If there is just one possibility, it's the right one
+                    #Return a solved sudoku with it
+                    if possibility_count == 1:
+                        new_rows = deepcopy(self.rows)
+                        new_rows[y][x] = possible_numbers[0]
+                        return SudokuBoard(new_rows).solved()
+                    
+                    #Keep track of the tile with the least possibilities
+                    if possibility_count < least_possibilities_tile_possibility_count:
+                        least_possibilities_tile = (y, x)
+                        least_possibilities_tile_possibility_count = possibility_count
+                        least_possibilities_tile_possibilities = possible_numbers
+
+        #If with that process we completed the board, we are done
+        if self.is_complete():
+            return SudokuBoard(deepcopy(self.rows))
+
+        #If that was't enough, start making assumptions.
+        #If we end up with an unsolvable sudoku, the assumption was wrong
+
+        #For each possibility of the tile with the least possibilities
+        for possibility in least_possibilities_tile_possibilities:
+            #Assume it's the right possibility and go from there
+            try:
+                assumption_rows = deepcopy(self.rows)
+                y = least_possibilities_tile[0]
+                x = least_possibilities_tile[1]
+                assumption_rows[y][x] = possibility
+                return SudokuBoard(assumption_rows).solved()
+            
+            #If we end up with an unsolvable Sudoku, try the next possibility
+            except UnsolvableSudokuException:
+                continue
+
+        #If every possibility gives an unsolvable sudoku, the board is unsolvable
+        raise UnsolvableSudokuException
---
+++ tests.py
@@ -0,0 +1,187 @@
+from solver import SudokuBoard, InvalidSudokuException,UnsolvableSudokuException
+
+def test_exceptions():
+    try:
+        s = SudokuBoard([[],[],[]])
+    except Exception as e:
+        assert type(e) == InvalidSudokuException
+
+    try:
+        s = SudokuBoard([[1,2,3,4,5,6,7,8,9],[1,2,3],[],[],[],[],[],[],[]])
+    except Exception as e:
+        assert type(e) == InvalidSudokuException
+
+    try:
+        SudokuBoard.subgridize([[],[]])
+    except Exception as e:
+        assert type(e) == ValueError
+        
+    print("Passed exceptions test")
+
+def test_simple_sudoku():
+    global s
+    assert SudokuBoard.subgridize([[1,1,1,2,2,2,3,3,3],
+                       [1,1,1,2,2,2,3,3,3],
+                       [1,1,1,2,2,2,3,3,3]]) == [[1, 1, 1, 1, 1, 1, 1, 1, 1],
+                                                 [2, 2, 2, 2, 2, 2, 2, 2, 2],
+                                                 [3, 3, 3, 3, 3, 3, 3, 3, 3]]
+
+    s = SudokuBoard([[8,   7,   6,     9,   None,None,  None,None,None],
+                     [None,1,   None,  None,None,6,     None,None,None],
+                     [None,4,   None,  3,   None,5,     8,   None,None],
+                     
+                     [4,   None,None,  None,None,None,  2,   1,   None],
+                     [None,9,   None,  5,   None,None,  None,None,None],
+                     [None,5,   None,  None,4,   None,  3,   None,   6],
+                     
+                     [None,2,   9,     None,None,None,  None,None,8   ],
+                     [None,None,4,     6,   9,   None,  1,   7,   3   ],
+                     [None,None,None,  None,None,1,     None,None,4   ]])
+    
+    assert s.subgrids() == [[8, 7, 6, None, 1, None, None, 4, None],
+                            [9, None, None, None, None, 6, 3, None, 5],
+                            [None, None, None, None, None, None, 8, None, None],
+                            [4, None, None, None, 9, None, None, 5, None],
+                            [None, None, None, 5, None, None, None, 4, None],
+                            [2, 1, None, None, None, None, 3, None, 6],
+                            [None, 2, 9, None, None, 4, None, None, None],
+                            [None, None, None, 6, 9, None, None, None, 1],
+                            [None, None, 8, 1, 7, 3, None, None, 4]]
+
+    assert s.cols() == [[8, None, None, 4, None, None, None, None, None],
+                        [7, 1, 4, None, 9, 5, 2, None, None],
+                        [6, None, None, None, None, None, 9, 4, None],
+                        [9, None, 3, None, 5, None, None, 6, None],
+                        [None, None, None, None, None, 4, None, 9, None],
+                        [None, 6, 5, None, None, None, None, None, 1],
+                        [None, None, 8, 2, None, 3, None, 1, None],
+                        [None, None, None, 1, None, None, None, 7, None],
+                        [None, None, None, None, None, 6, 8, 3, 4]]
+
+    assert s.possibilities() == {(0, 4): [1, 2],
+                               (0, 5): [2, 4],
+                               (0, 6): [4, 5],
+                               (0, 7): [2, 3, 4, 5],
+                               (0, 8): [1, 2, 5],
+                               (1, 0): [2, 3, 5, 9],
+                               (1, 2): [2, 3, 5],
+                               (1, 3): [2, 4, 7, 8],
+                               (1, 4): [2, 7, 8],
+                               (1, 6): [4, 5, 7, 9],
+                               (1, 7): [2, 3, 4, 5, 9],
+                               (1, 8): [2, 5, 7, 9],
+                               (2, 0): [2, 9],
+                               (2, 2): [2],
+                               (2, 4): [1, 2, 7],
+                               (2, 7): [2, 6, 9],
+                               (2, 8): [1, 2, 7, 9],
+                               (3, 1): [3, 6, 8],
+                               (3, 2): [3, 7, 8],
+                               (3, 3): [7, 8],
+                               (3, 4): [3, 6, 7, 8],
+                               (3, 5): [3, 7, 8, 9],
+                               (3, 8): [5, 7, 9],
+                               (4, 0): [1, 2, 3, 6, 7],
+                               (4, 2): [1, 2, 3, 7, 8],
+                               (4, 4): [1, 2, 3, 6, 7, 8],
+                               (4, 5): [2, 3, 7, 8],
+                               (4, 6): [4, 7],
+                               (4, 7): [4, 8],
+                               (4, 8): [7],
+                               (5, 0): [1, 2, 7],
+                               (5, 2): [1, 2, 7, 8],
+                               (5, 3): [1, 2, 7, 8],
+                               (5, 5): [2, 7, 8, 9],
+                               (5, 7): [8, 9],
+                               (6, 0): [1, 3, 5, 6, 7],
+                               (6, 3): [4, 7],
+                               (6, 4): [3, 5, 7],
+                               (6, 5): [3, 4, 7],
+                               (6, 6): [5, 6],
+                               (6, 7): [5, 6],
+                               (7, 0): [5],
+                               (7, 1): [8],
+                               (7, 5): [2, 8],
+                               (8, 0): [3, 5, 6, 7],
+                               (8, 1): [3, 6, 8],
+                               (8, 2): [3, 5, 7, 8],
+                               (8, 3): [2, 7, 8],
+                               (8, 4): [2, 3, 5, 7, 8],
+                               (8, 6): [5, 6, 9],
+                               (8, 7): [2, 5, 6, 9]}
+
+    assert s.solved().rows == [[8, 7, 6,  9, 1, 4,  5, 3, 2],
+                               [3, 1, 5,  2, 8, 6,  7, 4, 9],
+                               [9, 4, 2,  3, 7, 5,  8, 6, 1],
+                             
+                               [4, 3, 8,  7, 6, 9,  2, 1, 5],
+                               [6, 9, 1,  5, 2, 3,  4, 8, 7],
+                               [2, 5, 7,  1, 4, 8,  3, 9, 6],
+                             
+                               [1, 2, 9,  4, 3, 7,  6, 5, 8],
+                               [5, 8, 4,  6, 9, 2,  1, 7, 3],
+                               [7, 6, 3,  8, 5, 1,  9, 2, 4]]
+    
+    assert s.is_complete() == False
+    assert s.is_solved() == False
+
+    assert s.possible_numbers(1, 0) == [2, 3, 5, 9]
+    assert s.possible_numbers(7, 5) == [2, 8]
+    assert s.possible_numbers(7, 1) == [8]
+
+    print("Basic functionality passed")
+
+def test_solved_sudoku():
+    solved = SudokuBoard([[7,3,5, 6,1,4, 8,9,2],
+                         [8,4,2, 9,7,3, 5,6,1],
+                         [9,6,1, 2,8,5, 3,7,4],
+                         
+                         [2,8,6, 3,4,9, 1,5,7],
+                         [4,1,3, 8,5,7, 9,2,6],
+                         [5,7,9, 1,2,6, 4,3,8],
+                         
+                         [1,5,7, 4,9,2, 6,8,3],
+                         [6,9,4, 7,3,8, 2,1,5],
+                         [3,2,8, 5,6,1, 7,4,9]])
+
+    assert solved.is_complete() == True
+    assert solved.is_solved() == True
+
+    print("Solved sudoku functionality passed")
+
+def test_hard_sudoku():
+    global hard_sudoku
+    hard_sudoku = SudokuBoard([[None,3   ,None, 2   ,None,None, None,None,6   ],
+                               [None,None,None, None,None,9   , None,None,4   ],
+                               [7   ,6   ,None, None,None,None, None,None,None],
+                               
+                               [None,None,None, None,5   ,None, 7   ,None,None],
+                               [None,None,None, None,None,1   , 8   ,6   ,None],
+                               [None,5   ,None, 4   ,8   ,None, None,9   ,None],
+                               
+                               [8   ,None,None, None,None,None, None,None,None],
+                               [None,None,None, None,7   ,6   , None,None,None,],
+                               [None,7   ,5   , None,None,8   , 1   ,None,None]])
+
+    assert hard_sudoku.solved().rows == [[4, 3, 8,  2, 1, 5,  9, 7, 6],
+                                         [5, 2, 1,  7, 6, 9,  3, 8, 4],
+                                         [7, 6, 9,  8, 3, 4,  5, 1, 2],
+                                    
+                                         [9, 8, 2,  6, 5, 3,  7, 4, 1],
+                                         [3, 4, 7,  9, 2, 1,  8, 6, 5],
+                                         [1, 5, 6,  4, 8, 7,  2, 9, 3],
+                                    
+                                         [8, 1, 4,  5, 9, 2,  6, 3, 7],
+                                         [2, 9, 3,  1, 7, 6,  4, 5, 8],
+                                         [6, 7, 5,  3, 4, 8,  1, 2, 9]]
+
+    print("Hard sudoku solving test passed")
+
+def test_all():
+    test_exceptions()
+    test_simple_sudoku()
+    test_solved_sudoku()
+    test_hard_sudoku()
+
+if __name__ == "__main__":
+    test_all()
